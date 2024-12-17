5cb644d9cd7f930bb9cc668118269498ae86cc35
Amr1234-ux <m40770099@gmail.com>
2024-12-17 10:11:12 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,30 @@
+
+# Copyright (C) 2020-2024 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## Inherit from the common tree
+include device/samsung/a25x-common/BoardConfigCommon.mk
+
+## Inherit from the proprietary configuration
+include vendor/samsung/a25x/BoardConfigVendor.mk
+
 DEVICE_PATH := device/samsung/a25x
 
-## Inherit proprietary vendor configuartion
-include vendor/samsung/a25x/BoardConfigVendor.mk
+## Kernel
+TARGET_KERNEL_CONFIG := s5e8825-a25x_defconfig
+
+## Manifest
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/device_manifest.xml
+
+## Properties
